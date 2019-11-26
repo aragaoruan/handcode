268409389bed194ce00e27d44fdf5fd9e3c22da5
Ruan Aragao <aragao.ruan@gmail.com>
2019-11-26 16:55:03 -0300

    styles

---
+++ src/styles/colors.js
@@ -0,0 +1,19 @@
+export default {
+  backgroundcolor: '#9cb8b780',
+
+  primary: {
+    color: '#00ae9d',
+    lightColor: '#59e1ce',
+    darkColor: '#007e6f',
+    textColor: '#fafafa',
+  },
+
+  secondary: {
+    color: '#003c4d',
+    lightColor: '#386679',
+    darkColor: '#001725',
+    textColor: '#ffffff',
+  },
+
+  transparent: 'rgba(0, 0, 0, 0)',
+};
