5c3a82e48bebd032e2696f6eb72f318588c861ea
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-11-27 11:23:47 -0300

    added communit and profile page

---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components/native';
+import {colors, fonts} from '../../styles';
+
+export const Container = styled.View`
+  padding: 30px;
+  background: #fff;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 40px;
+  font-weight: bold;
+  color: ${colors.primary.color};
+  margin: 5px 0 10px 0;
+`;
+
+export const AvatarArea = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const DataInfo = styled.View`
+  width: 100%;
+  justify-content: flex-start;
+  margin-top: 20px;
+`;
+
+export const Row = styled.View`
+  flex-direction: row;
+  width: 100%;
+  margin-bottom: 10px;
+  border-bottom-color: #ccc;
+  border-bottom-width: 2px;
+  padding-bottom: 10px;
+`;
+
+export const Label = styled.Text`
+  font-size: 25px;
+  font-weight: bold;
+  width: 150px;
+  line-height: 30px;
+`;
+
+export const Value = styled.Text`
+  font-size: 25px;
+  width: 150px;
+  line-height: 30px;
+`;
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,14 @@ import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import Welcome from './pages/Welcome';
 import Journey from './pages/Journey';
+import Community from './pages/Community';
+import Profile from './pages/Profile';
 
 const TabNavigator = createBottomTabNavigator({
-  Home: Welcome,
-  Jornada: Journey,
+  // Home: Welcome,
+  // Jornada: Journey,
+  // Comunidade: Community,
+  Perfil: Profile,
 });
 
 export default createAppContainer(TabNavigator);
